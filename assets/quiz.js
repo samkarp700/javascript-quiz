80fa0bdb4bbb2f7ade58e46b62ea4194f505b2a5
Sam Karp <sfcypret@gmail.com>
2022-06-27 20:02:24 -0400

    initial commit, files set up, outline and plan created

---
+++ assets/quiz.js
@@ -0,0 +1,18 @@
+//GIVEN I am taking a code quiz
+
+//1. 
+//WHEN I click the start button (clickable button)
+//THEN a timer starts (create timer)
+//and I am presented with a question (question on page, not prompts)
+
+//2. WHEN I answer a question (multiple choice, 4 options, clickable)
+// THEN I am presented with another question 
+
+//3. WHEN I answer a question incorrectly- keep score
+//THEN time is subtracted from the clock - 
+
+//4. WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+
+//5. WHEN the game is over
+//THEN I can save my initials and score (local storage)
