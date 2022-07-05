34a49629afd559b4171d2fc4648c7cd42974f144
Sam Karp <sfcypret@gmail.com>
2022-07-04 20:10:25 -0400

    added html page for quiz, array for quiz questions, working on buttons

--- assets/quiz.js
+++ assets/quiz.js
@@ -17,7 +17,70 @@
 //5. WHEN the game is over
 //THEN I can save my initials and score (local storage)
 
-function quizGreet() 
-   
-    
+var highScore = document.querySelector("#highscore.btn");
+var questionsEl = document.querySelector("#question");
+var answersEl = document.querySelector("#answers");
+var beginQuiz = document.querySelector("#begin-quiz");
+var counterEl = 60;
 
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+
+var questions = [
+    {
+        question: 'What is the coding question?',
+        choice1: 'answer 1', 
+        choice2: 'answer 2',
+        choice3: 'answer 3', 
+        choice4: 'answer 4', 
+        answer: 2,
+    },
+    {
+        question: 'What is the coding question 2?',
+        choice1: 'answer 1', 
+        choice2: 'answer 2',
+        choice3: 'answer 3', 
+        choice4: 'answer 4', 
+        answer: 4,
+    }, 
+    {
+        question: 'What is the coding question 3 ?',
+        choice1: 'answer 1', 
+        choice2: 'answer 2',
+        choice3: 'answer 3', 
+        choice4: 'answer 4', 
+        answer: 3,
+    }, 
+    {
+        question: 'What is the coding question 4?',
+        choice1: 'answer 1', 
+        choice2: 'answer 2',
+        choice3: 'answer 3', 
+        choice4: 'answer 4', 
+        answer: 1,
+    },
+    {
+        question: 'What is the coding question 5?',
+        choice1: 'answer 1', 
+        choice2: 'answer 2',
+        choice3: 'answer 3', 
+        choice4: 'answer 4', 
+        answer: 2,
+    }
+]
+// Greeting page - begin button 
+var beginQuiz = function() {
+var buttonEl = document.querySelector("#begin-quiz");
+buttonEl.textContent = "Begin Quiz";
+buttonEl.className = "begin-quiz-btn";
+console.log("you clicked the button");
+};
+
+
+
+
+
+
+    
